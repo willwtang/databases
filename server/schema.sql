cf06ff7a9888cadeeb6f5c3fababb0445d26bbdc
willwtang <willwyatttang@gmail.com>
2016-05-13 16:34:32 -0700

    created database

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+ roomID int NOT NULL AUTO_INCREMENT
+ ,roomName varchar(30)
+ ,PRIMARY KEY (roomID)
+);
+
+CREATE TABLE users (
+ userID int NOT NULL AUTO_INCREMENT
+ ,userName varchar(30)
+ ,PRIMARY KEY (userID)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  messageID int NOT NULL AUTO_INCREMENT
+  ,messageText varchar(140)
+  ,messageSentTime TIMESTAMP
+  ,roomID_Rooms int
+  ,userID_Users int
+  ,FOREIGN KEY (roomID_Rooms) REFERENCES rooms(roomID)
+  ,FOREIGN KEY (userID_Users) REFERENCES users(userID)
+  ,PRIMARY KEY (messageID)
 );
 
 /* Create other tables and define schemas for them here! */
@@ -12,6 +31,6 @@ CREATE TABLE messages (
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u root < /Users/student/Desktop/2016-04-databases/server/schema.sql
  *  to create the database and the tables.*/
 
